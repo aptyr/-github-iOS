32db3f67f159ba9e6cbe8b7a6f57df700306793b
Artur Matusiak <mail@matusiakartur.com>
2017-03-12 21:12:15 +0100

    Project init

---
+++ #github-ios/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  #github-ios
+//
+//  Created by Artur on 07/03/2017.
+//  Copyright © 2017 Artur Matusiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
